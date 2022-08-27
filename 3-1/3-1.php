971a2ef2e27f8065193c88b9b4888a002f62e37c
yomotiz <112060874+yomotiz@users.noreply.github.com>
2022-08-27 21:16:08 +0900

    FizzBuzz問題
    FizzBuzz問題の提出

---
+++ 3-1/3-1.php
@@ -0,0 +1,20 @@
+<?php
+
+for($i=1;$i<100;$i++) {
+    
+    if($i%3 == 0 && $i%5 == 0){
+        echo "FizzBuzz!!";
+        echo "<br>"; 
+    }elseif($i%3 == 0){
+        echo "Fizz!";
+        echo "<br>"; 
+    }elseif($i%5 == 0){
+        echo "Buzz!";
+        echo "<br>"; 
+    }else{
+        echo $i;
+        echo "<br>";
+    }
+}
+
+?>
