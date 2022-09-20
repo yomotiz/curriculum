9a264df931ea2f4ff400e8e089046de1f69f2427
yomotiz <112060874+yomotiz@users.noreply.github.com>
2022-09-20 13:20:11 +0900

    3-3修正提出
    3-3の課題の修正を提出

--- 3-3/result.php
+++ 3-3/result.php
@@ -1,16 +1,16 @@
 <?php
 $num = $_POST['num'];
-echo "$num <br>";
+//echo "$num <br>";
 
 //入力された数字の数を取得
 $kazu = strlen($num);
-echo "$kazu <br>";
+//echo "$kazu <br>";
 //入力された文字列の抜き出す番号を選択
 $r = mt_rand(1, $kazu);
-echo "$r <br>";
+//echo "$r <br>";
 //選んだ番号の数字を抜く
 $s_num = $num[$r-1];
-echo "$s_num <br>";
+//echo "$s_num <br>";
 
 //運勢の決定
 //０：凶、１〜３：小吉、４〜６、中吉、７〜８：吉、９：大吉
